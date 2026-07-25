1d47ebd912ef1f53a8b3f115dcbcb24a0c57e712
Aleksey Kulikov <67376842+AGKulikov@users.noreply.github.com>
2026-07-25 11:01:29 +0300

    fix: import scenario nullability annotation

--- app/src/main/java/dezz/status/widget/ScenarioSettingsActivity.java
+++ app/src/main/java/dezz/status/widget/ScenarioSettingsActivity.java
@@ -22,6 +22,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
