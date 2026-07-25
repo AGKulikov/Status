a08067b68474ae145c135b88701bdf77e5820ccd
Aleksey Kulikov <67376842+AGKulikov@users.noreply.github.com>
2026-07-25 15:51:05 +0300

    Fix HA1085 candidate compilation

--- app/src/main/java/dezz/status/widget/DriverFavoritesSettingsActivity.java
+++ app/src/main/java/dezz/status/widget/DriverFavoritesSettingsActivity.java
@@ -19,6 +19,7 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
